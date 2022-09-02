3228b679d1a7bbfc349c8c1efbe449eba6101583
bakerbrittani <bakerbrittani@yahoo.com>
2022-09-02 15:12:19 -0700

    final version

--- static/js/charts.js
+++ static/js/charts.js
@@ -59,12 +59,13 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
+    console.log(data);
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultsArray = samplesArray.filter(desiredSample => desiredSample.id == sample);
-    var metaArray = data.metadata.filter(desiredMetadata => desiredMetadata.id == sample);
+    var resultsArray = samplesArray.filter(obj => obj.id == sample);
+    var metaArray = data.metadata.filter(obj => obj.id == sample);
 
     //  5. Create a variable that holds the first sample in the array.
     var results = resultsArray[0]
@@ -78,30 +79,32 @@ function buildCharts(sample) {
     // 7. Create the yticks for the bar chart.
     // Combined with other deliverable variables
 
-    var yticks = ids.slice(0, 10).reverse().map(function(label){return `OTU ${label}`});
+    var yticks = ids.slice(0, 10).map(function(label){return `OTU ${label}`}).reverse();
     var xticks = samval.slice(0, 10).reverse();
     var labels = labels.slice(0, 10).reverse();
     var wfreq = metaResults.wfreq;
 
     // 8. Create the trace for the bar chart. 
-    var barData = {
+    var barData = [{
         x: xticks,
         y: yticks,
         type: 'bar',
         orientation: 'h',
         text: labels
-      };
+      }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
         title: "Top 10 Bacteria Cultures Found",
+        plot_bgcolor: "srgba(255, 255, 255, 0.452)", 
+        bargap: 0.05,
         margin: {
           t: 30,
           l: 120
         }
-      };
+        };
 
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", [barData], barLayout); 
+    Plotly.newPlot("bar", barData, barLayout); 
 
 //Deliverable 2: Bubble Chart
 // Bar and Bubble charts
@@ -124,7 +127,9 @@ function buildCharts(sample) {
       automargin: true,
       xaxis: { title: "OTU ID" },
       showlegend: false,
-      hovermode: "closest"
+      hovermode: "closest",
+      height: 350, 
+      width: 1000
       };
   
       // 3. Use Plotly to plot the data with the layout.
@@ -154,9 +159,9 @@ function buildCharts(sample) {
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-      width: 600, 
-      height: 500, 
-      margin: { t: 50, r: 25, l: 25, b: 5 } 
+      width: 500, 
+      height: 400, 
+      margin: { t: 0, b: 0 } 
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
